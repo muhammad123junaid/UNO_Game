8238ce0801731e8dabde8d937a6273ebf8d6f98d
muhammad123junaid <241443@students.au.edu.pk>
2025-06-15 00:24:47 +0500

    Update ASCII.h

--- ASCII.h
+++ ASCII.h
@@ -1,20 +1,23 @@
 #ifndef ASCII_H
 #define ASCII_H
 
-#include "Card.h"
+#include "Card.hpp"
 #include <vector>
 #include <string>
 #include <iostream>
 
-namespace UNO {
+namespace UNO
+{
 
-class ASCIIArt {
-public:
-    static void renderCardBox(const Card& card, size_t index = static_cast<size_t>(-1));
-    static void renderGameState(const Card& topCard, const std::vector<Card>& hand);
-    static void printWinBanner(); 
-    static void printLoseBanner();
-};
+    class ASCIIArt
+    {
+    public:
+        static void renderCardBox(const Card &card, size_t index = static_cast<size_t>(-1));
+        static void renderGameState(const Card &topCard, const std::vector<Card> &hand);
+        static void printWinBanner();
+        static void printLoseBanner();
+        static void printStartingCard(const Card &card);
+    };
 
-} 
+}
 #endif
